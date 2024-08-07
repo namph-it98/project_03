6c784090da9e3fe79b44bf29fdd8b53d6e2c5a4e
Nam Phạm <namph.it98@gmail.com>
2024-08-07 15:12:40 +0700

    first commit

--- udagram-api-user/src/sequelize.ts
+++ udagram-api-user/src/sequelize.ts
@@ -10,10 +10,4 @@ export const sequelize = new Sequelize({
 
   'dialect': config.dialect,
   'storage': ':memory:',
-  'dialectOptions': { //< Add this
-    'ssl': {
-       'require': true,
-       'rejectUnauthorized': false
-    }
- }
 });
