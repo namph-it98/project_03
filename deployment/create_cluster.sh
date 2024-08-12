147ef31f4b058db2db608955b624a4abecf6ed05
Nam Phạm <namph.it98@gmail.com>
2024-08-12 22:17:57 +0700

    update v21

--- deployment/create_cluster.sh
+++
@@ -1,6 +0,0 @@
-eksctl create cluster \
-  --name uda-prj3-final1 \
-  --region us-east-1 \
-  --nodegroup-name grp-nodes \
-  --node-type t3.medium \
-  --managed
--- deployment/update_config.sh
+++
@@ -1,1 +0,0 @@
-aws eks --region us-west-2 update-kubeconfig --name uda-prj3-final
